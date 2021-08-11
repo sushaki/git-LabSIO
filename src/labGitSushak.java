42e063f5cfb740511ea36362021c6994812f91a9
sushaki <ivan.alex.sushak@gmail.com>
2021-08-11 22:28:36 +0300

    Add new Method "doSmth"

--- src/labGitSushak.java
+++ src/labGitSushak.java
@@ -1,10 +1,14 @@
 public class labGitSushak {
+    void doSmth (){
+        int d = 4+4;
+    }
     public static void main(String[] args) {
         int a = 3;
         int b = 8;
         System.out.println("a + b = " + a + b);
 
         String tetra = "Tetra";
+
 
     }
 
